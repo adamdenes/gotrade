bbe9439c249196db5fa6d2490686844bb06d886d
adamdenes <adam.denes@icloud.com>
2023-09-19 13:32:28 +0200

    adding simple /search route

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -10,6 +11,7 @@ func NewRouter() http.Handler {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/search", searchHandler)
 
 	return mux
 }
@@ -25,6 +27,7 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		"./web/templates/base.tmpl.html",
 		"./web/templates/pages/chart.tmpl.html",
 		"./web/templates/partials/script.tmpl.html",
+		"./web/templates/partials/search_bar.tmpl.html",
 	}
 
 	// Parse the HTML template
@@ -41,4 +44,16 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
 		return
 	}
+}
+
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	// Send 404 if destination is not `/`
+	if r.Method != http.MethodGet {
+		w.Header().Set("Allow", http.MethodGet)
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	symbol := r.FormValue("symbol")
+	fmt.Fprintf(w, "You were searching for '%v'\n", symbol)
 }
