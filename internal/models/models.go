b54b0bba2f52668def4a820face08d73724c1459
adamdenes <adam.denes@icloud.com>
2023-09-22 16:06:07 +0200

    creating struct for KLINE

--- internal/models/models.go
+++ internal/models/models.go
@@ -2,15 +2,15 @@ package models
 
 type Kline struct {
 	OpenTime              int64  `json:"open_time"`
-	OpenPrice             string `json:"open_price"`
-	HighPrice             string `json:"high_price"`
-	LowPrice              string `json:"low_price"`
-	ClosePrice            string `json:"close_price"`
+	Open                  string `json:"open"`
+	High                  string `json:"high"`
+	Low                   string `json:"low"`
+	Close                 string `json:"close"`
 	Volume                string `json:"volume"`
 	CloseTime             int64  `json:"close_time"`
-	QuoteAssetVolume      string `json:"quote_asset_volume"`
-	NumberOfTrades        int    `json:"number_of_trades"`
-	TakerBuyBaseAssetVol  string `json:"taker_buy_base_asset_volume"`
-	TakerBuyQuoteAssetVol string `json:"taker_buy_quote_asset_volume"`
-	UnusedField           string `json:"unusedField"`
+	QuoteAssetVolume      string `json:"quote_volume"`
+	NumberOfTrades        int    `json:"count"`
+	TakerBuyBaseAssetVol  string `json:"taker_buy_volume"`
+	TakerBuyQuoteAssetVol string `json:"taker_buy_quote_volume"`
+	Ignore                string `json:"-"`
 }
