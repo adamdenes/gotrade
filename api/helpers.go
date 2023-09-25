4b6a6f0b95a1d843c8182ee11a99e6a2fee4e4c2
adamdenes <adam.denes@icloud.com>
2023-09-25 17:41:41 +0200

    (feat): adding JSON response writer

--- api/helpers.go
+++ api/helpers.go
@@ -1,14 +1,13 @@
 package api
 
 import (
-	"archive/zip"
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"html/template"
 	"io"
 	"net/http"
 	"runtime/debug"
+	"strings"
 )
 
 func (s *Server) serverError(w http.ResponseWriter, err error) {
@@ -46,28 +45,52 @@ func (s *Server) render(w http.ResponseWriter, data any) {
 	}
 }
 
-func downloadAndReadZIP(url string) (*zip.ReadCloser, error) {
-	// Send HTTP GET request to download the ZIP file
-	response, err := http.Get(url)
+// Download kline data with 1s interval in ZIP format and reads it into memory
+// for further processing (dumping into database)
+// func downloadAndReadZIP(url string) (*zip.ReadCloser, error) {
+// 	// Send HTTP GET request to download the ZIP file
+// 	response, err := http.Get(url)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	defer response.Body.Close()
+
+// 	// Read the response body into a byte buffer
+// 	buffer := new(bytes.Buffer)
+// 	_, err = io.Copy(buffer, response.Body)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	// New reader for the in-memory ZIP archive
+// 	zipReader, err := zip.NewReader(bytes.NewReader(buffer.Bytes()), int64(buffer.Len()))
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	return &zip.ReadCloser{Reader: *zipReader}, nil
+// }
+
+func GET(url string) (*http.Response, error) {
+	resp, err := http.Get(url)
 	if err != nil {
 		return nil, err
 	}
-	defer response.Body.Close()
+	return resp, nil
+}
 
-	// Read the response body into a byte buffer
-	buffer := new(bytes.Buffer)
-	_, err = io.Copy(buffer, response.Body)
-	if err != nil {
-		return nil, err
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	// []byte slices are not converting correctly, so I need to type switch
+	switch data := v.(type) {
+	case []byte:
+		_, err := w.Write(data)
+		return err
+	default:
+		return json.NewEncoder(w).Encode(data)
 	}
-
-	// New reader for the in-memory ZIP archive
-	zipReader, err := zip.NewReader(bytes.NewReader(buffer.Bytes()), int64(buffer.Len()))
-	if err != nil {
-		return nil, err
-	}
-
-	return &zip.ReadCloser{Reader: *zipReader}, nil
 }
 
 // ----------------- REST -----------------
@@ -88,12 +111,85 @@ func downloadAndReadZIP(url string) (*zip.ReadCloser, error) {
    GET /api/v3/uiKlines
 */
 
-func GET(url string) (*http.Response, error) {
-	resp, err := http.Get(url)
+/*
+GET /api/v3/klines
+
+	Kline/candlestick bars for a symbol.
+	Klines are uniquely identified by their open time.
+
+	symbol 		STRING 	YES
+	fromId 		LONG 	NO 	id to get aggregate trades from INCLUSIVE.
+	startTime 	LONG 	NO 	Timestamp in ms to get aggregate trades from INCLUSIVE.
+	endTime 	LONG 	NO 	Timestamp in ms to get aggregate trades until INCLUSIVE.
+	limit 		INT 	NO 	Default 500; max 1000.
+
+
+    If startTime and endTime are not sent, the most recent klines are returned.
+*/
+
+func getKlines(s string, i string) ([]byte, error) {
+	var sb strings.Builder
+	sb.WriteString("https://data-api.binance.vision/api/v3/klines?")
+	sb.WriteString(fmt.Sprintf("symbol=%s", s))
+	sb.WriteString(fmt.Sprintf("&interval=%s", i))
+
+	resp, err := http.Get(sb.String())
 	if err != nil {
 		return nil, err
 	}
-	return resp, nil
+	defer resp.Body.Close()
+
+	// Check the HTTP status code
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("HTTP request failed with status code %d", resp.StatusCode)
+	}
+
+	// Parse the JSON response
+	r, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return r, nil
+}
+
+/*
+GET /api/v3/uiKlines
+
+	The request is similar to klines having the same parameters and response.
+	uiKlines return modified kline data, optimized for presentation of candlestick charts.
+
+	symbol 		STRING 	YES
+	fromId 		LONG 	NO 	id to get aggregate trades from INCLUSIVE.
+	startTime 	LONG 	NO 	Timestamp in ms to get aggregate trades from INCLUSIVE.
+	endTime 	LONG 	NO 	Timestamp in ms to get aggregate trades until INCLUSIVE.
+	limit 		INT 	NO 	Default 500; max 1000.
+*/
+
+func getUiKlines(s string, i string) ([]byte, error) {
+	var sb strings.Builder
+	sb.WriteString("https://data-api.binance.vision/api/v3/uiKlines?")
+	sb.WriteString(fmt.Sprintf("symbol=%s", s))
+	sb.WriteString(fmt.Sprintf("&interval=%s", i))
+
+	resp, err := http.Get(sb.String())
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	// Check the HTTP status code
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("HTTP request failed with status code %d", resp.StatusCode)
+	}
+
+	// Parse the JSON response
+	r, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return r, nil
 }
 
 /*
@@ -101,7 +197,7 @@ GET /api/v3/exchangeInfo
 
 	OPTIONS (not all of them):
 	- no parameter	"https://api.binance.com/api/v3/exchangeInfo"
-	- symbol			"https://api.binance.com/api/v3/exchangeInfo?symbol=BNBBTC"
+	- symbol		"https://api.binance.com/api/v3/exchangeInfo?symbol=BNBBTC"
 	- symbols		'https://api.binance.com/api/v3/exchangeInfo?symbols=["BTCUSDT","BNBBTC"]'
 	- permissions	"https://api.binance.com/api/v3/exchangeInfo?permissions=SPOT"
 
@@ -111,7 +207,7 @@ GET /api/v3/exchangeInfo
 
 func getSymbols() ([]string, error) {
 	// Send a GET request to the Binance API endpoint
-	resp, err := http.Get("https://api.binance.com/api/v3/exchangeInfo")
+	resp, err := http.Get("https://data-api.binance.vision/api/v3/exchangeInfo")
 	if err != nil {
 		return nil, err
 	}
