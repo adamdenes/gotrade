c919566203fa7c4c27254864a3e7c28aaaf67fce
adenes <adam.denes@icloud.com>
2023-11-27 21:16:45 +0100

    feat: separating GET and POST order API responses

--- internal/models/models.go
+++ internal/models/models.go
@@ -284,6 +284,29 @@ const (
 	EXPIRED_IN_MATCH             = "EXPIRED_IN_MATCH"
 )
 
+type GetOrderResponse struct {
+	Symbol                  string `json:"symbol"`
+	OrderID                 int64  `json:"orderId"`
+	OrderListID             int64  `json:"orderListId"`
+	ClientOrderID           string `json:"clientOrderId"`
+	Price                   string `json:"price"`
+	OrigQty                 string `json:"origQty"`
+	ExecutedQty             string `json:"executedQty"`
+	CummulativeQuoteQty     string `json:"cummulativeQuoteQty"`
+	Status                  string `json:"status"`
+	TimeInForce             string `json:"timeInForce"`
+	Type                    string `json:"type"`
+	Side                    string `json:"side"`
+	StopPrice               string `json:"stopPrice"`
+	IcebergQty              string `json:"icebergQty"`
+	Time                    int64  `json:"time"`
+	UpdateTime              int64  `json:"updateTime"`
+	IsWorking               bool   `json:"isWorking"`
+	WorkingTime             int64  `json:"workingTime"`
+	OrigQuoteOrderQty       string `json:"origQuoteOrderQty"`
+	SelfTradePreventionMode string `json:"selfTradePreventionMode"`
+}
+
 type PostOrderResponse struct {
 	Symbol                  string `json:"symbol"`
 	OrderID                 int64  `json:"orderId"`
